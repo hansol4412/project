df1f3f1d4d3b6f7aec2465c8b33910a74b88ccf8
hansol <hansol4412@naver.com>
2020-12-17 13:05:15 +0900

    chap08

---
+++ sp5-web/ex02/src/main/java/org/zerock/domain/BoardVO.java
@@ -0,0 +1,13 @@
+package org.zerock.domain;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Long bno;
+	private String title;
+	private String content;
+	private String witer;
+	private Date regdate;
+	private Date updateDate;
+}
