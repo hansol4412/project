1991c532775342c78542479f773f5a6821cd4677
hansol <hansol4412@naver.com>
2021-01-13 10:17:38 +0900

    chap15

--- sp5-web/ex02/src/main/java/org/zerock/controller/BoardController.java
+++ sp5-web/ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -65,9 +65,11 @@ public class BoardController {
 		if (service.modify(board)) {
 		rttr.addFlashAttribute("result", "success");
 		}
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
-		return "redirect:/board/list";
+		//rttr.addAttribute("pageNum", cri.getPageNum());
+		//rttr.addAttribute("amount", cri.getAmount());
+		//rttr.addAttribute("type", cri.getType());
+		//rttr.addAttribute("keyword", cri.getKeyword());
+		return "redirect:/board/list" + cri.getListLink();
 	}
 	
 	@PostMapping("/remove")
@@ -76,8 +78,8 @@ public class BoardController {
 		if (service.remove(bno)) {
 		rttr.addFlashAttribute("result", "success");
 		}
-		rttr.addAttribute("pageNum", cri.getPageNum());
-		rttr.addAttribute("amount", cri.getAmount());
-		return "redirect:/board/list";
+		//rttr.addAttribute("pageNum", cri.getPageNum());
+		//rttr.addAttribute("amount", cri.getAmount());
+		return "redirect:/board/list" + cri.getListLink();
 	}
 }
