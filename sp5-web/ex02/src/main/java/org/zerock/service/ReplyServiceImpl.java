18159b789bfcecee40c44a0454d5efbf737acb06
hansol <hansol4412@naver.com>
2021-01-28 10:11:37 +0900

    chap20

--- sp5-web/ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
+++ sp5-web/ex02/src/main/java/org/zerock/service/ReplyServiceImpl.java
@@ -2,7 +2,9 @@ package org.zerock.service;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.Mapping;
+import org.zerock.mapper.BoardMapper;
 import org.zerock.mapper.ReplyMapper;
 import org.zerock.domain.ReplyVO;
 import org.zerock.domain.Criteria;
@@ -17,9 +19,14 @@ public class ReplyServiceImpl implements ReplyService {
 	@Setter(onMethod_= @Autowired)
 	private ReplyMapper mapper;
 	
+	@Setter(onMethod_= @Autowired)
+	private BoardMapper boardMapper;
+	
+	@Transactional
 	@Override
 	public int register(ReplyVO vo){
 		log.info("register---------" + vo);
+		boardMapper.updateReplyCnt(vo.getBno(), 1);
 		return mapper.insert(vo);
 	}
 	
@@ -35,9 +42,12 @@ public class ReplyServiceImpl implements ReplyService {
 		return mapper.update(vo);
 	}
 	
+	@Transactional
 	@Override
 	public int remove(Long rno){
 		log.info("delete-----------------------"+rno);
+		ReplyVO vo = mapper.read(rno);
+		boardMapper.updateReplyCnt(vo.getBno(), -1);
 		return mapper.delete(rno);
 	}
 	
