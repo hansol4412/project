1ddffe6c9310ea2701772b076d21607a30b87d33
hansol <hansol4412@naver.com>
2021-01-30 12:41:01 +0900

    chap22

--- sp5-web/ex05/src/main/java/org/zerock/controller/UploadController.java
+++ sp5-web/ex05/src/main/java/org/zerock/controller/UploadController.java
@@ -1,14 +1,29 @@
 package org.zerock.controller;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.UUID;
+import java.util.List;
+
 
 import lombok.extern.log4j.Log4j;
+import net.coobird.thumbnailator.Thumbnailator;
+import org.zerock.domain.AttachFileDTO;
 
 @Controller
 @Log4j
@@ -42,23 +57,71 @@ public class UploadController {
 		log.info("upload ajax");
 	}
 	
-	@PostMapping("/uploadAjaxAction")
-	public void uploadAjaxPost(MultipartFile[] uploadFile) {
+	private String getFolder() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		String str = sdf.format(date);
+		return str.replace("-", File.separator);
+	}
+	
+	private boolean checkImageType(File file) {
+		try {
+			String contentType = Files.probeContentType(file.toPath());
+			return contentType.startsWith("image");
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
+	@PostMapping(value="/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<AttachFileDTO>> uploadAjaxPost(MultipartFile[] uploadFile) {
+		List<AttachFileDTO> list = new ArrayList<AttachFileDTO>();
 		String uploadFolder="C:\\upload";
+		String uploadFolderPath = getFolder();
+		//make folder-------------
+		File uploadPath = new File(uploadFolder, uploadFolderPath);
+		log.info("uploadPath:" +  uploadPath);
+		if(uploadPath.exists() == false) {
+			uploadPath.mkdirs();
+		}
+		//make yyyy/MM/dd folder
 		
 		for(MultipartFile multipartFile : uploadFile) {
+			
+			AttachFileDTO attachDTO = new AttachFileDTO();
+			
 			log.info("----------------------------");
 			log.info("Upload File Name:" + multipartFile.getOriginalFilename());
 			log.info("Upload File Size:" + multipartFile.getSize());
 			
-			File saveFile = new File(uploadFolder, multipartFile.getOriginalFilename());
+			String uploadFileName = multipartFile.getOriginalFilename();
+			attachDTO.setFileName(uploadFileName);
+			UUID uuid = UUID.randomUUID();
+			uploadFileName = uuid.toString() + "_" + uploadFileName;
+			
+			
 			
 			try {
+				File saveFile = new File(uploadPath, uploadFileName);
 				multipartFile.transferTo(saveFile);
+				attachDTO.setUuid(uuid.toString());
+				attachDTO.setUploadPath(uploadFolderPath);
+				
+				//이미지 파일인지 체크
+				if(checkImageType(saveFile)) {
+					attachDTO.setImage(true);
+					FileOutputStream thumbnail = new FileOutputStream(new File(uploadPath, "s_" + uploadFileName ));
+					Thumbnailator.createThumbnail(multipartFile.getInputStream(), thumbnail, 100, 100 );
+					thumbnail.close();
+				}
+				list.add(attachDTO);
 			} catch(Exception e) {
 				log.error(e.getMessage());
 			}
 		}
+		return new ResponseEntity<List<AttachFileDTO>>(list,HttpStatus.OK);
 	}
 	
 
--- sp5-web/ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
+++ sp5-web/ex05/src/main/webapp/WEB-INF/views/uploadAjax.jsp
@@ -23,6 +23,20 @@
 			
 	<script>
 		$(document).ready(function(){
+			var regex = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+			var maxSize = 5242880;
+			function checkExtension(fileName, fileSize){
+				if(fileSize>maxSize){
+					elert("파일 사이즈 초과");
+					return false;
+				}
+				if(regex.test(fileName)){
+					alert("해당 종류의 파일은 업로드 할 수 없습니다");
+					return false;
+				}
+				return true;
+			}
+			
 			$("#uploadBtn").on("click", function(e){
 				var formData = new FormData();
 				var inputFile = $("input[name='uplaodFile']");
@@ -30,18 +44,23 @@
 				console.log(files);
 				
 				for(var i=0; i<files.length; i++){
+					if(!checkExtension(files[i].name, files[i].size)){
+						return false;
+					}
+					
 					formData.append("uploadFile", files[i]);
 				}
 				
 				$.ajax({
-					type: 'post',
 					url: '/uploadAjaxAction',
-					data: formData,
-					contentType : false,
 					processData : false,
-					success : function(result) {
-						alert("uploaded");
-						}
+					contentType : false,
+					data: formData,
+						type: 'post',
+						dataType : 'json',
+						success : function(result) {
+							alert("uploaded");
+							}
 				});
 			});
 		});
